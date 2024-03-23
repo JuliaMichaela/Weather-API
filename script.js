4f6331fb10f6c7bc838cebe340ab79988f7f926f
JuliaMichaela <juliaperez@yandex.ru>
2024-03-23 23:59:12 +0300

    deleted console.log

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ form.onsubmit = function (e) {
         return response.json()
     }).then((data) => {
         console.log (data);
-        console.log (data);
-        console.log (data);
+        
 
 
         
